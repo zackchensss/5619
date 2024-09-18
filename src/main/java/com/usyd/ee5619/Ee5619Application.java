bdf1cac58ce3eda748a54022377d2fce75a03d63
Yifan <yche0158@uni.sydney.edu.au>
2024-09-18 21:26:59 +1000

    Initialize the project

---
+++ src/main/java/com/usyd/ee5619/Ee5619Application.java
@@ -0,0 +1,13 @@
+package com.usyd.ee5619;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ee5619Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ee5619Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/usyd/ee5619/Ee5619ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usyd.ee5619;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ee5619ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
