29db510a552e39224a6f43aea3244a395a56c3b7
Yifan <yche0158@uni.sydney.edu.au>
2024-10-17 23:52:13 +1100

    fix a bug

--- src/main/java/com/usyd/ee5619/Mapper/HomePageMapper.java
+++ src/main/java/com/usyd/ee5619/Mapper/HomePageMapper.java
@@ -14,13 +14,13 @@ public interface HomePageMapper {
     String getWeeklyStudyTime(int userId);
     @Select("SELECT COALESCE((SELECT course FROM course_status WHERE user_id = #{userId} AND complete_num = 0 LIMIT 1), 'math')")
     String getRecommendCourse(int userId);
-    @Select("SELECT SUM(complete_num) FROM course_status WHERE user_id = #{userId} AND course = 'math'")
+    @Select("SELECT COALESCE(SUM(complete_num),0) FROM course_status WHERE user_id = #{userId} AND course = 'math'")
     int getMathCompletion(int userId);
 
-    @Select("SELECT SUM(complete_num) FROM course_status WHERE user_id = #{userId} AND course = 'reading'")
+    @Select("SELECT COALESCE(SUM(complete_num),0) FROM course_status WHERE user_id = #{userId} AND course = 'reading'")
     int getReadingCompletion(int userId);
 
-    @Select("SELECT SUM(complete_num) FROM course_status WHERE user_id = #{userId} AND course = 'writing'")
+    @Select("SELECT COALESCE(SUM(complete_num),0) FROM course_status WHERE user_id = #{userId} AND course = 'writing'")
     int getWritingCompletion(int userId);
     @Select("SELECT DATE_FORMAT(date, '%m-%d') AS month,SUM(TIME_TO_SEC(STR_TO_DATE(study_time, '%i:%s'))) AS time FROM study_progress WHERE user_id = #{userId} GROUP BY month")
     List<StudyTimeByMonth> getStudyTimeByMonth(int userId);
